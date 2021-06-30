413fa63865595e6b481f5e0193b56b6b194d7366
Cerotinez <86720875+Cerotinez@users.noreply.github.com>
2021-06-30 18:42:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Em à!',
+    introTitle: 'Chị à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: ':33',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Yêu anh mà <<333',
-    btnNo: 'Không, em chỉ coi anh là bạn :3',
+    title: 'Phải chăng chị đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng chị đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yêu bé mà <<333',
+    btnNo: 'Không, chị chỉ coi bé là người thân :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnReply: 'Gửi cho bé <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Làm cho vui thôi, Đừng ảo tưởng',
     btnAccept: ':D',
-    messLink: 'https://www.facebook.com/dangpham.cero/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/dangpham.cero/' 
 }
